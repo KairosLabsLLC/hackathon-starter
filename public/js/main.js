6178757ad23fae441b12a15615217a229e29c3c6
Raymond Sapida <ray.sapida@gmail.com>
2017-07-13 14:19:05 -0700

    Add front panels to cover a three additional ingenico api endpoints
    - These front end components expect routes for:
    GET /api/ingenico-prodcts
    GET /api/ingenico-products/:id
    POST /api/ingenico-payment

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,62 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
+
+  $('#ingenico-products').on('click', function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    var codeResponse = $('#ingenico-products-response')
+
+    $.ajax({
+      url: '/api/ingenico-products?amount=10000',
+      method: 'GET',
+      success: function(data){
+        codeResponse.empty()
+        codeResponse.append('<pre>'+ JSON.stringify(data, undefined, 2) +'</pre>')
+      },
+      error: function(err){
+        console.warn(err)
+      }
+    })
+
+  })
+
+  $('#ingenico-specific-product').on('click', function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    var codeResponse = $('#ingenico-product-response')
+
+    $.ajax({
+      url: '/api/ingenico-products/1',
+      method: 'GET',
+      success: function(data){
+        codeResponse.empty()
+        codeResponse.append('<pre>'+ JSON.stringify(data, undefined, 2) +'</pre>')
+      },
+      error: function(err){
+        console.warn(err)
+      }
+    })
+
+  })
+
+  $('#ingenico-payment').on('submit', function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    var codeResponse = $('#ingenico-payment-response')
+
+    $.ajax({
+      url: '/api/ingenico-payment',
+      method: 'POST',
+      data: $('#ingenico-payment').serialize(),
+      success: function(data){
+        codeResponse.empty()
+        codeResponse.append('<pre>'+ JSON.stringify(data, undefined, 2) +'</pre>')
+      },
+      error: function(err){
+        console.warn(err)
+      }
+    })
+
+  })
 
 });
